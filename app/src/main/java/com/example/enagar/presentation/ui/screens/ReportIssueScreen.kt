fb2b3e6635b73f7246d48358c0d70b929d3b4322
It-Saurabh001 <09saurabh07@gmail.com>
2025-09-17 13:50:24 +0530

    aborted merge

--- app/src/main/java/com/example/enagar/presentation/ui/screens/ReportIssueScreen.kt
+++ app/src/main/java/com/example/enagar/presentation/ui/screens/ReportIssueScreen.kt
@@ -50,6 +50,7 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.SettingsClient
 import kotlinx.coroutines.launch
 import java.util.jar.Manifest
 import kotlin.contracts.contract
