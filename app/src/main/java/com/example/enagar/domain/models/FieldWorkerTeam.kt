6a407447ed0715914d5f59cc4608a8ac0bbd8e65
Krishna-Sinoria <krishnasinoria@gmail.com>
2026-07-05 16:58:06 +0530

    update ui

---
+++ app/src/main/java/com/example/enagar/domain/models/FieldWorkerTeam.kt
@@ -0,0 +1,16 @@
+package com.example.enagar.domain.models
+
+data class FieldWorkerTeam(
+
+
+    val _id: String,
+
+    val name: String,
+
+    val email: String,
+
+    val members: String,
+
+    val status: String
+)
+
